b461118f885007b73c5db35f1993e90cdbf3fe43
Maxwell Heeschen <gorpmaster@gmail.com>
2021-01-20 20:10:26 -0600

    Remove unused lines

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,7 @@
 extends Node2D
 
-
-signal stage_changed(position, scale)
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	idle()
-
 
 func walk():
 	$AnimatedSprite.play("walk")
